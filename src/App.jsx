4c3be08a52b9b5222acc370aa0c2acb1e3e9ea7d
Mahmudul Islam <122343396+Mahmudulislamshuvo@users.noreply.github.com>
2025-12-28 17:37:21 +0600

    Implement routing and add NotFound component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
+import Profile from "./components/Profile";
+import NotFound from "./components/NotFound";
+
 const App = () => {
-  return <div>swerwer</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} exact />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registration" element={<Registration />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
