7e789bc20659a8f1170954783668bbc18c97149f
Mahmudul Islam <122343396+Mahmudulislamshuvo@users.noreply.github.com>
2025-12-31 22:53:59 +0600

    Refactor PrivateRoutes to include ProfileProvider; add useProfile hook for context access

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,23 +1,26 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import Navbar from "../components/Navbar";
+import ProfileProvider from "../provider/ProfileProvider";
 
 const PrivateRoutes = () => {
   const { auth } = useAuth();
 
-  if (!auth.user) {
+  if (!auth.user || !auth.authToken) {
     return <Navigate to="/login" />;
   }
 
   return (
-    <div>
-      <Navbar />
-      <main className="mx-auto max-w-5xl py-8">
-        <div className="container">
-          <Outlet />
-        </div>
-      </main>
-    </div>
+    <>
+      <ProfileProvider>
+        <Navbar />
+        <main className="mx-auto max-w-5xl py-8">
+          <div className="container">
+            <Outlet />
+          </div>
+        </main>
+      </ProfileProvider>
+    </>
   );
 };
 
